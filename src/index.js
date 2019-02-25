b55557b8ba1de0c374a18cf6503710aa321622cb
Valeryia Himzheuskaya <valimisael@yahoo.com>
2019-02-25 18:15:14 +0300

    feat: check brackets secuence

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var brackets = str.split('');
+
+  if (brackets.length % 2 != 0) return false;
+
+  var openers = [],
+      closers = [];
+
+  for (var i=0; i<bracketsConfig.length; i++) {
+    openers.push(bracketsConfig[i][0]);
+    closers.push(bracketsConfig[i][1]);
+  } 
+  
+  var matchingOpener,
+      arrOfBrackets = [];
+
+  for (var i = 0; i < brackets.length; i++) {
+    var bracket = brackets[i];
+    for (var j = 0; j < openers.length; j++) {
+      if (bracket == openers[j]) {
+        arrOfBrackets.push(bracket);
+      }
+      if (bracket == closers[j]) {
+        matchingOpener = openers[closers.indexOf(bracket)];
+        if (arrOfBrackets.pop() != matchingOpener) {
+          return false;
+        }
+      }
+    }
+  }
+  return arrOfBrackets.length == 0 ? true : false;
 }
